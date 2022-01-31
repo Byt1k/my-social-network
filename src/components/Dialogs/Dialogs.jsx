f29a81ce5e58cdb45511cba9104e861156ce655d
Byt1k <byt1kip82@gmail.com>
2022-01-31 23:50:12 +0300

    Array.map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,50 @@
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = props => {
+    let path = "/dialogs/" + props.id;
+    return <NavLink to={path} className={navData => navData.isActive ? `${s.item} ${s.active}` : s.item}>{props.name}</NavLink>
+}
+
+const Message = props => {
+    return <div className={s.message}>{props.message}</div>
+}
 
 const Dialogs = props => {
+    let dialogsData = [
+        {id: 1, name: 'Nikolas'},
+        {id: 2, name: 'Julia'},
+        {id: 3, name: 'Hanna'},
+        {id: 4, name: 'David'},
+        {id: 5, name: 'Mary'},
+        {id: 6, name: 'Loren'},
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hi!"'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'}
+    ]
+
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsList}>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+            </div>
+            <div className={s.chat}>
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
+                <Message message={messagesData[3].message} />
+                <Message message={messagesData[4].message} />
+            </div>
         </div>
     );
 }
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -3,19 +3,19 @@ import CreatePost from "./CreatePost/CreatePost";
 import Post from "./Post/Post";
 
 const Posts = () => {
-    const background = {
-        background: 'url(https://res.cloudinary.com/worldpackers/image/upload/c_fill,f_auto,q_auto,w_1024/v1/guides/article_cover/fl4bzxx2pvifrjtc4l6x) no-repeat center center / cover'
-    }
-    const imageMartin = {
-        background: 'url(https://www.onlineheadway.com/assets/img/courses/06.jpg) no-repeat center center / cover'
-    }
+    let postsData = [
+        {id: 1, date: "25 sep 2021", likesCount: 13, text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto optio illo pariatur molestiae sapiente similique, velit impedit dolorum quam ducimus libero explicabo voluptatum officiis. Ipsum esse in sunt autem ipsam!"},
+        {id: 2, date: "23 sep 2021", likesCount: 7, text: "Hey, why nobody love me?"},
+        {id: 3, date: "21 sep 2021", likesCount: 913, text: "This is my first post. Now I'm with you!"}
+    ]
+
+    let postsElements = postsData.map(post => <Post date={post.date} likesCount={post.likesCount} text={post.text}/>);
+
     return (
         <div className={s.posts}>
             <div className={s.title}>My posts</div>
             <CreatePost />
-            <Post date="25 sep 2021" likesCount={13} text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto optio illo pariatur molestiae sapiente similique, velit impedit dolorum quam ducimus libero explicabo voluptatum officiis. Ipsum esse in sunt autem ipsam!"/>
-            <Post date="23 sep 2021" likesCount={7} text="Hey, why nobody love me?"/>
-            <Post date="21 sep 2021" likesCount={913} text="This is my first post. Now I'm with you!"/>
+            {postsElements}
         </div>
     );
 }
