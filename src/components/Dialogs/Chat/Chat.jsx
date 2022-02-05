00592925c53c945ca34f99fd9f1c570c58f7cfd7
Byt1k <byt1kip82@gmail.com>
2022-02-05 21:53:47 +0300

    deleted CreateMessage component

--- src/components/Dialogs/Chat/Chat.jsx
+++ src/components/Dialogs/Chat/Chat.jsx
@@ -1,16 +1,29 @@
 import {Message} from "./Message/Message";
 import s from "./Chat.module.css";
-import {CreateMessage} from "./CreateMessage/CreateMessage";
 import React from "react";
+import {sendMessageCreator, updateNewMessageTextCreator} from "../../../redux/dialogs-reducer";
 
 export const Chat = props => {
 
     let messages = props.messages.map(message => <Message message={message.message}/>)
 
+    const sendMessage = () => {
+        props.dispatch(sendMessageCreator());
+    }
+
+    const onMessageChange = e => {
+        let newMessageText = e.target.value;
+        props.dispatch(updateNewMessageTextCreator(newMessageText))
+    }
+
     return (
         <div className={s.chat}>
             {messages}
-            <CreateMessage dispatch={props.dispatch} newMessageText={props.newMessageText}/>
+            {/*<CreateMessage dispatch={props.dispatch} newMessageText={props.newMessageText}/>*/}
+            <div className={s.createMessage}>
+                <textarea onChange={onMessageChange} placeholder={'Your message...'} value={props.newMessageText}></textarea>
+                <button onClick={sendMessage}>Send</button>
+            </div>
         </div>
     );
 }
