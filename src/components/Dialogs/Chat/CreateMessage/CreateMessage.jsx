9bfeadcf36a29db5ab83c4e15f92d0af23a93cf7
Byt1k <byt1kip82@gmail.com>
2022-02-05 14:35:45 +0300

    fixed react ref

--- src/components/Dialogs/Chat/CreateMessage/CreateMessage.jsx
+++ src/components/Dialogs/Chat/CreateMessage/CreateMessage.jsx
@@ -5,20 +5,19 @@ import {sendMessageActionCreator, updateNewMessageTextActionCreator} from "../..
 
 
 export const CreateMessage = props => {
-    let newMessage = React.createRef();
 
     const sendMessage = () => {
         props.dispatch(sendMessageActionCreator());
     }
 
-    const onMessageChange = () => {
-        let newMessageText = newMessage.current.value;
+    const onMessageChange = e => {
+        let newMessageText = e.target.value;
         props.dispatch(updateNewMessageTextActionCreator(newMessageText))
     }
 
     return (
         <div className={s.createMessage}>
-            <textarea onChange={onMessageChange} placeholder={'Your message...'} ref={newMessage} value={props.newMessageText}></textarea>
+            <textarea onChange={onMessageChange} placeholder={'Your message...'} value={props.newMessageText}></textarea>
             <button onClick={sendMessage}>Send</button>
         </div>
     );
--- src/components/Profile/Posts/CreatePost/CreatePost.jsx
+++ src/components/Profile/Posts/CreatePost/CreatePost.jsx
@@ -3,18 +3,17 @@ import React from "react";
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../../redux/state";
 
 const CreatePost = props => {
-    let newPostTextarea = React.createRef();
 
     const addPost = () => props.dispatch(addPostActionCreator());
 
-    const changeNewPostText = () => {
-        let newPostText = newPostTextarea.current.value;
+    const changeNewPostText = e => {
+        let newPostText = e.target.value;
         props.dispatch(updateNewPostTextActionCreator(newPostText));
     }
 
     return (
         <div className={s.newPost}>
-            <textarea onChange={changeNewPostText} placeholder="Your news..." ref={newPostTextarea} value={props.newPostText}/>
+            <textarea onChange={changeNewPostText} placeholder="Your news..." value={props.newPostText}/>
             <button onClick={addPost}>Send</button>
         </div>
     );
