e47dd453a09df1753519a389d1520514deb4e8a2
Byt1k <byt1kip82@gmail.com>
2022-02-23 13:11:36 +0300

    added redux-from for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,39 @@
-const Login = props => {
+import s from './Login.module.css'
+import {Field, reduxForm} from "redux-form";
+
+const LoginForm = props => {
     return (
-        <h1>Login Page</h1>
+        <form onSubmit={props.handleSubmit}>
+            <div className={s.inputBlock}>
+                <p>Login</p>
+                <Field component={'input'} name={'login'} />
+            </div>
+            <div className={s.inputBlock}>
+                <p>Password</p>
+                <Field component={'input'} name={'password'} type={'password'}/>
+            </div>
+            <div className={s.checkboxBlock}>
+                <Field component={'input'} name={'rememberMe'} type={'checkbox'} id={'rememberMe'}/>
+                <label htmlFor={'rememberMe'}>Remember Me</label>
+            </div>
+            <button>Sign In!</button>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+
+const Login = props => {
+
+    const onSubmit = formData => {
+        console.log(formData);
+    }
+
+    return (
+        <div className={s.loginPage}>
+            <h1>Sign In!</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
     )
 }
 
