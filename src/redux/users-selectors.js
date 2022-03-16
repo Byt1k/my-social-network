20524339c62305fd08b28be86f6f9e96c8474b38
Byt1k <byt1kip82@gmail.com>
2022-03-16 14:29:21 +0300

    added reselect

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,14 @@
-export const getUsers = state => {
+import {createSelector} from "reselect";
+
+const getUsersSelector = state => {
     return state.usersPage.users
 }
+
+// Пример использлования reselect
+export const getUsers = createSelector(getUsersSelector, users => {
+    return users.filter(u => u);
+})
+//
 
 export const getPageSize = state => {
     return state.usersPage.pageSize
